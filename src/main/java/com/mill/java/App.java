031ccc451cc8109ebb58814123f5861a79730b33
mill1983 <mill1983@126.com>
2017-05-07 11:44:31 +0800

    first commit

---
+++ src/main/java/com/mill/java/App.java
@@ -0,0 +1,13 @@
+package com.mill.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
